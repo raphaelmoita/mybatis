974a80ab0220ae4479c662d1b1f2619c000b91f5
Raphael Moita <raphael.moita@gmail.com>
2019-05-11 13:49:43 +0200

    fix enum list

--- src/main/java/org/moita/App.java
+++ src/main/java/org/moita/App.java
@@ -3,7 +3,9 @@ package org.moita;
 import org.apache.ibatis.session.SqlSession;
 import org.moita.config.MyBatisConfig;
 import org.moita.domain.Company;
+import org.moita.domain.Employee;
 import org.moita.mapper.CompanyMapper;
+import org.moita.mapper.EmployeeMapper;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,6 +19,10 @@ public class App
             CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
             List<Company> companies = companyMapper.findAll();
             System.out.println(companies);
+
+            EmployeeMapper employeeMapper = session.getMapper(EmployeeMapper.class);
+            List<Employee> employees = employeeMapper.findAll();
+            System.out.println(employees);
 
         } catch (IOException e) {
             e.printStackTrace();
